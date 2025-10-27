a584d7301e3d7fc1d3d02d2ef3189162db98d93f
justice-1297-dev <justicelee.dev@gmail.com>
2025-10-26 21:39:12 -0500

    Delete Desktop directory

--- Desktop/leet_code/majority_element.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def majorityElement(self, nums: list[int]) -> int:
-        nums.sort()
-        n = len(nums)
-        return nums[n//2]
-test = Solution()
-print(test.majorityElement(nums = [9, 8, 8, 9, 6, 7, 9]))
-print(test.majorityElement(nums = [2,2,1,1,1,2,2]))
